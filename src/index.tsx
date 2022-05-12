93c330c3c65572df0903aed01f96f80e65d85e85
kerbin <kerbin@sweetcherrytech.com>
2022-05-12 18:46:05 -0400

    initial commit

---
+++ src/index.tsx
@@ -0,0 +1,25 @@
+import React, { useEffect } from 'react';
+import { Widget, addResponseMessage } from 'react-chat-widget';
+
+// Delete me
+export const Chataverse = () => {
+  useEffect(() => {
+    addResponseMessage('Welcome to this **awesome** chat!');
+  }, []);
+
+  const handleNewUserMessage = (newMessage:any) => {
+    console.log(`New message incoming! ${newMessage}`);
+    // Now send the message throught the backend API
+  };
+
+
+  return (
+    <div className="App">
+      <Widget
+        handleNewUserMessage={handleNewUserMessage}
+        title="My new awesome title"
+        subtitle="And my cool subtitle"
+      />
+    </div>
+  );
+};
